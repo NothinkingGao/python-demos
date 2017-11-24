f6100fc98095956b7d60ec7c5c3b666419d8d543
gaoming <mingming.gao@alphainsight.ai>
2017-11-24 16:38:23 +0800

    类变量和实例变量

---
+++ class.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+
+class Person:
+    name="aaa"
+ 
+p1=Person()
+p2=Person()
+p1.name="bbb"
+print p1.name  # bbb
+print p2.name  # aaa
+print Person.name  # aaa
+
+class Person:
+    name=[]
+ 
+p1=Person()
+p2=Person()
+p1.name.append(1)
+print p1.name  # [1]
+print p2.name  # [1]
+print Person.name  # [1]
